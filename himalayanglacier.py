9bbb3b8bb7bcb947f35428a668e896181eb3f80e
Bibekkun <bibekrai655@gmail.com>
2023-03-23 11:48:17 +0545

    added cost and trip grade

--- himalayanglacier.py
+++ himalayanglacier.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 datalist=[]
-urls=['https://www.himalayanglacier.com/trips/nepal/activities/trekking-and-hiking/']
+urls=['https://www.himalayanglacier.com/trips/annapurna-base-camp-trek/']
    
 
 for url in urls:
@@ -12,19 +12,22 @@ for url in urls:
 
     soup=BeautifulSoup(html_content,'html.parser')
 
-    post_divs=soup.find_all("div", {"class": "col-sm-12 col-md-12 col-lg-12 col-xl-12"})
 
-    for post_div in post_divs:
-        Trek=post_div.find('div',{"class":'card-body'}).find('h4').text.strip()
-        #time=post_div.find('span',{"class":'duration'}).find("i").text.strip()
-        #cost=post_div.find('div',{"class":'price'}).find("p").text.strip()
-        contact_or_book_your_trip='https://www.himalayanglacier.com'
-        data={
-            'Trek':Trek,
-            #'Cost':cost,
-            #'Time':time,
-            'Contact or Book your Trip':contact_or_book_your_trip
+
+    Trek=soup.find('h1',{"class":'tourmaster-single-header-title'}).text.strip()
+    time=soup.find('h1',{"class":'tourmaster-single-header-title'}).find('span').text.strip()
+    cost=soup.find('div',{"class":'price'}).find("p").text.strip()
+    trip_grade=soup.find('span',{"class":'hg-tooltip-text'}).text.strip()
+    altitude=soup.find('div',{"class":'div.col-sm-12.col-md-12.col-lg-12.col-xl-12.overview-block.p-0'}).find('div',{"class":'txt-info'})[5].find_all('span').text.strip()
+    contact_or_book_your_trip='https://www.himalayanglacier.com'
+    data={
+        'Trek':Trek.split("-")[0],
+        'Cost':cost,
+        'Time':time.removeprefix("-"),
+        'Trip Grade':trip_grade,
+        "Max Altitude":altitude,
+        'Contact or Book your Trip':contact_or_book_your_trip
         }
-        datalist.append(data)
+    datalist.append(data)
 df=pd.DataFrame(datalist)
 df.to_csv('himalayanglacier.csv',index=True)
